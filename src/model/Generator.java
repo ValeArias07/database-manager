53c7f8aa6ad974506efb4d6f6605c106234ee228
Valentina Arias Parra <valeariasparra2001@gmail.com>
2020-10-29 21:04:31 -0500

    An special import added: The InputReader Class. Also, there is a new method to calculate the height giving a date.

--- src/model/Generator.java
+++ src/model/Generator.java
@@ -1,12 +1,18 @@
 package model;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+
+import imports.InputReader;
 
 
 /**
@@ -24,9 +30,12 @@ import java.util.ArrayList;
 public class Generator {
 	///// Data in Arrays. (In order to save memory).
 	public static final String[] COUNTRIES= {"Argentina","Bahamas", "Barbados","Belice","Bolivia","Brasil","Canadá","Chile","Colombia","Costa Rica","Cuba","Dominica","Ecuador","El Salvador","Estados Unidos","Granada","Guatemala","Guyana","Haití","Honduras","Jamaica","México","Nicaragua","Panamá","Paraguay","Perú","República Dominicana","Santa Lucía","Surinam","Trinidad y Tobago","Uruguay","Venezuela"};
-	public static final double PERCENT[]= {45267449, 393893, 287437,398845,11700207,212821986, 37799407,19144605,50976248,5102158,11325391,17688599, 6491923, 331341050,112614,17971382,787215,11426356,9931333,2963429,129166028,6638075,4326296,7147553,33050211,10866667,183774,587541,1400283,3475842,28421581};
-	public static final LocalDate INITIALYEAR= LocalDate.of(2020, 1, 1);
+	public static final double PERCENTCOUNTRIES[]= {4.43,0.04,0.03,0.04,1.14,20.84,3.7,1.87,4.99,0.5,1.11,0.01,1.73,32.45,0.64,0.01,1.76,0.08,1.12,0.97,0.29,12.64,0.65,0.42,0.7,3.24,0.28,1.06,0.02,0.06,0.14,0.34,2.79};
+	public static final double PERCENTPEOPLE[]= {18.62 ,13.12 ,39.29 ,12.94 ,16.03};
 	public static final double MAXHEIGHT=200.0;
+	
+	public static String[] names;
+	public static String[] surnames;
 	
 	//// Data's Adress
 	public static final String MNAMES ="data/datasets/MaleNames.txt";
@@ -39,41 +48,109 @@ public class Generator {
 	public int men;
 	public long amountOfPeople;
 	
-	public double percentFulled[]; //// Atributo para medir la cantidad de gente generada en un determinado pais.
+	public double percentByCountries[];//// Atributo para medir la cantidad de gente generada en un determinado pais.
+	public int peopleByCountrie[];
+	public int percentPeopleForAges[];
+	public int ages[];
 	
-	public Generator() {
-		this.percentFulled = new double[PERCENT.length];
+	public Generator() throws IOException {
+		this.percentByCountries= new double[PERCENTCOUNTRIES.length];
+		this.percentPeopleForAges= new int[PERCENTPEOPLE.length];
+		this.peopleByCountrie=new int[PERCENTCOUNTRIES.length];
+		storeNames();
+		storeSurnames();
 		men=0;
 		women=0;
 		amountOfPeople=0;
 	}	
 	
-	public void generate(long amountToGenerate) throws IOException {
-		String name="";
+	public void generate(int amountToGenerate) throws IOException {
+		int nameIndex=0, surnameIndex=0, bornIndex=0;
 		LocalDate date;
-		boolean gender;
-		long time=System.currentTimeMillis();
-		ArrayList<Person> peopleGenerated=new ArrayList<Person>();/// Se almacenan aca por si quiere ver como quedan los objetos con el metodo toString.
-		BufferedReader readerId= new BufferedReader(new FileReader(ID)), readerFName= new BufferedReader(new FileReader(FNAMES)), readerMName= new BufferedReader(new FileReader(MNAMES)), readerSurname= new BufferedReader(new FileReader(SURNAMES));
+		generatePercentAge(amountToGenerate);
+		generatePercentPeopleByCountrie(amountToGenerate);
+		bornDate(amountToGenerate);
+		long time=System.currentTimeMillis();		
+		//ArrayList<Person> peopleGenerated=new ArrayList<Person>();/// Se almacenan aca por si quiere ver como quedan los objetos con el metodo toString.
+	
 		while(amountOfPeople<amountToGenerate) { ///Ciclo para que se generen el numero de personas
-				gender=gender(); /// Llama al metodo gender. Segun el gender, se toma un nombre masculino o femenino.
 				
-				///Condicional para los nombres.
-				name=(gender==false? readerFName.readLine()+" "+readerSurname.readLine():readerMName.readLine()+" "+readerSurname.readLine());
 				///Creacion de fecha.
-				date=bornDate();
-				
+				date=getBornDate(bornIndex);
+
 				///Creacion de persona.
-				peopleGenerated.add(new Person(readerId.readLine(),name,gender,date,height(),COUNTRIES[getCountry()],"defined"));
+				Person p=new Person("",names[nameIndex]+" "+surnames[nameIndex],gender(),date,height(date),COUNTRIES[getCountry()],"defined");
 				
 				//atribbuto para ver si se crean la cantidad de personas correcta.
+				nameIndex=(nameIndex>names.length? 0:+1);
+				surnameIndex=(surnameIndex>surnames.length?0:1);
+				bornIndex++;
 				amountOfPeople++;
 		}
-		readerId.close();
-		readerFName.close();
-		readerMName.close();
+
 		System.out.println(System.currentTimeMillis()-time);
 	}
+
+	public  void storeSurnames() throws IOException {
+		surnames=new String[100000];
+		InputReader readerS = new InputReader(new FileInputStream(SURNAMES));
+		int max=0;
+		while(max<=names.length-1) {
+			surnames[max]=readerS.nextLine();
+			max++;
+		}
+	}
+	public void storeNames() throws IOException {
+		names=new String[100000];
+		InputReader readerF = new InputReader(new FileInputStream(FNAMES));
+		InputReader readerM = new InputReader(new FileInputStream(MNAMES));
+		int max=0;
+		while(max<=names.length-1) {
+			if(max%2==0) {
+				names[max]=readerM.nextLine();
+			}else{
+				names[max]=readerF.nextLine();
+			}
+			max++;
+		}
+	}
+	
+	public void generatePercentAge(long population) {
+		for (int i = 0; i < PERCENTPEOPLE.length; i++) {
+			percentPeopleForAges[i]=(int)(PERCENTPEOPLE[i]*population)/100;
+		}
+	}
+	
+	public void generatePercentPeopleByCountrie(long population) {
+		for (int i = 0; i < PERCENTCOUNTRIES.length; i++) {
+			percentByCountries[i]=(int)(PERCENTCOUNTRIES[i]*population)/100;
+		}
+	}
+	
+
+	public int getCountry() {
+		int cnty=(int)(Math.random()*30);
+		boolean available=false;
+		while(!available) {
+			if(PERCENTPEOPLE[cnty]>=percentByCountrie[cnty]) { /// Esto lo que hace es que si un pais ya tiene su limite lleno, no asigne mas personas a este pais.
+				available=true;
+				percentFulled[cnty]=percentFulled[cnty]+1;
+			}else { /// De ya estar lleno el pais, se cambia a otro aleatorio.
+				cnty=(int)(Math.random()*30);
+			}
+		}
+		return cnty;
+	}
+	
+	public LocalDate getBornDate(int index) {
+		int years=ages[index];
+		int month=years%12;
+		int day=years%30;
+		month=(month==0? 1:month);
+		day=(day==0? 1:day);
+		return LocalDate.of(years, month, day);
+	}
+
 
 	public LocalDate bornDate() {
 		int random=(int)(Math.random()*80); /// Genera un numero aleatorio de 0 a 80.
@@ -82,6 +159,7 @@ public class Generator {
 		newDate=newDate.minus(random%12, ChronoUnit.MONTHS); //Igual que lo anterior
 		return newDate;
 	}
+	
 	
 	public boolean gender() {
 		//// Como se supone que debe haber una distribucion igual de mujeres y hombres, se creo este condicional.
@@ -97,25 +175,31 @@ public class Generator {
 		}
 	}
 	
-	public double height() {//// Aca tenia unos condicionales pero los quite.
-		return MAXHEIGHT-Math.random()*100;
+	public double height(LocalDate date) {//// Aca tenia unos condicionales pero los quite.
+		double random=Math.random()*180;
+		int sustraction=2020-date.getYear();
+		
+		if(sustraction<10) {
+			if(random>=100) 
+				random=100;
+			else if(random<50) 
+				random=30;
+		}else {
+			if(random<90) 
+				random=MAXHEIGHT-random;
+		}
+		return (double)Math.round(random * 100) / 100;
 	}
 	
-	public int getCountry() {
-		int cnty=(int)(Math.random()*30);
-		boolean available=false;
-		while(!available) {
-			if(PERCENT[cnty]>=percentFulled[cnty]) { /// Esto lo que hace es que si un pais ya tiene su limite lleno, no asigne mas personas a este pais.
-				available=true;
-				percentFulled[cnty]=percentFulled[cnty]+1;
-			}else { /// De ya estar lleno el pais, se cambia a otro aleatorio.
-				cnty=(int)(Math.random()*30);
-			}
-		}
-		return cnty;
-	}
+	
 	
 	public long getTotal() { ///Metodo para comprobar que efectivamente se crearon el numero n de personas.
 		return amountOfPeople;
+	}
+	
+	public void getPercentage() {
+		for (int i = 0; i < ages.length; i++) {
+			System.out.println(ages[i]);
+		}
 	}
 }
--- test/model/GeneratorTest.java
+++ test/model/GeneratorTest.java
@@ -1,23 +1,25 @@
 package model;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 public class GeneratorTest {
 	private Generator g;
 	
-	void setUp1(){
+	void setUp1() throws IOException{
 		g= new Generator();
 	}
 	@Test
 	public void generateTest() throws IOException {
 		setUp1();
-		g.generate(100000000);
-		System.out.println(g.getTotal());
-		/**
-		 * for (int i = 0; i < totalPerson.size(); i++) {
-		 * System.out.println(totalPerson.get(i).toString());
-		}
-		 */
-			
+	g.generate(1000000);
+	//g.getPercentage();
+	System.out.println(g.getTotal());
+	
 	}
 }
